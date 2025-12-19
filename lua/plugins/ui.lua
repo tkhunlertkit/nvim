fa80430f159bc7cb6df727eb6808c1a612ba6ab7
tkhunlertkit <tkhunlertkit@gmail.com>
2025-12-19 14:31:14 +0700

    change to nord theme instead of tokyo night

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,31 +2,20 @@
 -- UI enhancements: colorscheme, statusline, indentation guides, etc.
 
 return {
-  -- Tokyo Night Colorscheme
+  -- Nord Colorscheme
   {
-    "folke/tokyonight.nvim",
+    "shaunsingh/nord.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-      require("tokyonight").setup({
-        style = "night",
-        transparent = false,
-        terminal_colors = true,
-        styles = {
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-          sidebars = "dark",
-          floats = "dark",
+      require("nord").setup({
+        contrast = false,
+        borders = true,
+        errors = {
+          mode = "background",
         },
-        sidebars = { "qf", "help" },
-        day_brightness = 0.3,
-        hide_inactive_statusline = false,
-        dim_inactive = false,
-        lualine_bold = false,
       })
-      vim.cmd.colorscheme("tokyonight")
+      vim.cmd.colorscheme("nord")
     end,
   },
 
