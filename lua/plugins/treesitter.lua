351d636c48428144fbf3aeb3b0726656e80b1461
tkhunlertkit <tkhunlertkit@gmail.com>
2025-12-23 08:57:52 +0700

    enable treesitter, but still some errors

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,147 +1,63 @@
--- lua/plugins/treesitter.lua
--- Treesitter for syntax highlighting, indentation, and more
+-- lua/plugins/treesitter.lua - MINIMAL VERSION (No textobjects)
+-- Treesitter configuration without textobjects to avoid loading issues
 
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdate", "TSInstall" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "nvim-treesitter/nvim-treesitter-context",
-    },
     config = function()
       require("nvim-treesitter.configs").setup({
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        auto_install = true,
+
+        -- List of parsers to install
         ensure_installed = {
-          "bash",
-          "css",
-          "dockerfile",
-          "git_config",
-          "gitignore",
-          "go",
-          "html",
-          "javascript",
-          "json",
           "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "regex",
-          "rust",
-          "sql",
-          "toml",
-          "tsx",
-          "typescript",
           "vim",
           "vimdoc",
+          "python",
+          "javascript",
+          "typescript",
+          "tsx",
+          "json",
           "yaml",
+          "html",
+          "css",
+          "bash",
+          "markdown",
+          "markdown_inline",
+          "regex",
+          "sql",
+          "go",
+          "rust",
+          "c",
+          "cpp",
         },
-        sync_install = false,
-        auto_install = true,
-        ignore_install = {},
 
+        -- Highlighting configuration
         highlight = {
           enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
           additional_vim_regex_highlighting = false,
         },
 
+        -- Indentation based on treesitter
         indent = {
           enable = true,
-          disable = { "python" },
         },
 
+        -- Incremental selection
         incremental_selection = {
           enable = true,
           keymaps = {
             init_selection = "<C-space>",
             node_incremental = "<C-space>",
-            scope_incremental = "<C-s>",
-            node_decremental = "<M-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
           },
-        },
-
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["al"] = "@loop.outer",
-              ["il"] = "@loop.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-          },
-        },
-      })
-
-      -- Setup Treesitter context (shows function signature while scrolling)
-      require("treesitter-context").setup({
-        enable = true,
-        max_lines = 0,
-        min_window_height = 0,
-        line_numbers = true,
-        multiline_threshold = 20,
-        trim_scope = "outer",
-        mode = "cursor",
-        separator = nil,
-        zindex = 20,
-      })
-    end,
-  },
-
-  -- Rainbow parentheses
-  {
-    "hiphish/rainbow-delimiters.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      require("rainbow-delimiters").setup({
-        strategy = {
-          [""] = require("rainbow-delimiters").strategy["local"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          lua = "rainbow-blocks",
-        },
-        highlight = {
-          "RainbowDelimiterRed",
-          "RainbowDelimiterYellow",
-          "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
-          "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
         },
       })
     end,
